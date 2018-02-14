1b2503b494d4293279a7e490673ccc51379dd561
Dylan Wilson <dmw3714@gmail.com>
2018-02-14 17:29:10 -0500

    API first commit

---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Fisher.Bookstore.Api.Models
+{
+
+        public class Author
+        {
+           
+            public int AuthorId {get; set;}
+            public string Name {get; set;}
+
+
+
+            
+        }
+}
